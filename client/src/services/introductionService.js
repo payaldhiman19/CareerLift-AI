f0d6adb46fe24256a4f2006002eefe2603013a52
Payal dhiman <payaldhiman0742@gmail.com>
2026-07-18 13:59:30 +0530

    modified

--- client/src/services/introductionService.js
+++ client/src/services/introductionService.js
@@ -3,8 +3,8 @@ import api from "./api";
 
 export const generateIntroduction = async(resumeId)=>{
 
-    const response = await axios.post(
-        `http://localhost:5000/api/introduction/generate/${resumeId}`,
+    const response = await api.post(
+        `introduction/generate/${resumeId}`,
         {},
         {
             headers:{
